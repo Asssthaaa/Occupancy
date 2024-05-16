5ef8799c6c7e70a32bd6808d41fdbcedc71bd384
Asssthaaa <2101202014.aasthaverma@gmail.com>
2024-05-16 17:29:58 +0530

    Update main.py
    added port number

--- main.py
+++ main.py
@@ -152,3 +152,7 @@ def predict_occupancy():
 
 if __name__ == "__main__":
     app.run()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
